3c65370712405d39d98bf59e864ac2af1247001e
Hiram R. Chirino <chirino@apache.org>
2009-02-19 17:30:11 +0000

    Added a proto2 wire which uses the new externalizable style encoding/decoding.  Just so we can benchmark protobuf encoding vs. more standard java style externalizable encoding.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/sandbox/activemq-flow@745938 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/activemq/flow/Router.java
+++ src/test/java/org/apache/activemq/flow/Router.java
@@ -11,19 +11,21 @@ import org.apache.activemq.flow.Commands.Destination;
 import org.apache.activemq.flow.MockBroker.DeliveryTarget;
 
 public class Router {
-    final HashMap<Destination, Collection<DeliveryTarget>> lookupTable = new HashMap<Destination, Collection<DeliveryTarget>>();
+    final HashMap<String, Collection<DeliveryTarget>> lookupTable = new HashMap<String, Collection<DeliveryTarget>>();
 
     final synchronized void bind(DeliveryTarget dt, Destination destination) {
-        Collection<DeliveryTarget> targets = lookupTable.get(destination);
+        String key = destination.getName();
+        Collection<DeliveryTarget> targets = lookupTable.get(key);
         if (targets == null) {
             targets = new ArrayList<DeliveryTarget>();
-            lookupTable.put(destination, targets);
+            lookupTable.put(key, targets);
         }
         targets.add(dt);
     }
 
     final void route(ISourceController<Message> source, Message msg) {
-        Collection<DeliveryTarget> targets = lookupTable.get(msg.getDestination());
+        String key = msg.getDestination().getName();
+        Collection<DeliveryTarget> targets = lookupTable.get(key);
         if( targets == null ) 
             return;
         for (DeliveryTarget dt : targets) {
