418bc15b19a796a5d26b6243f2d6a0669f47a6f5
Hiram R. Chirino <chirino@apache.org>
2010-10-29 01:21:53 +0000

    Converted the website from webgen based generator to a scalate based generator.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-apollo/trunk@1028579 13f79535-47bb-0310-9956-ffa450edef68

---
+++ apollo-website/src/main/scala/Website.scala
@@ -0,0 +1,53 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.fusesource.scalate.RenderContext
+
+package
+
+/**
+ * <p>
+ * </p>
+ *
+ * @author <a href="http://hiramchirino.com">Hiram Chirino</a>
+ */
+object Website {
+
+  val project_name= "Apollo"
+  val project_slogan= "The scalable messaging broker"
+  val project_id= "apollo"
+  val project_jira_key= "APOLLO"
+  val project_issue_url= "https://issues.apache.org/activemq/browse/AMQ"
+  val project_forums_url= "http://activemq.2283324.n4.nabble.com/ActiveMQ-Dev-f2368404.html"
+  val project_wiki_url= "https://cwiki.apache.org/confluence/display/ACTIVEMQ/Index"
+  val project_logo= "/images/project-logo.png"
+  val project_version= "6.0-SNAPSHOT"
+  val project_snapshot_version= "6.0-SNAPSHOT"
+
+  val project_keywords= "messaging,stomp,jms,activemq,apollo"
+
+  // -------------------------------------------------------------------
+  val project_svn_url= "http://svn.apache.org/repos/asf/activemq/activemq-apollo"
+  val project_svn_trunk_url= project_svn_url +"/trunk"
+  val project_svn_branches_url= project_svn_url + "/branches"
+  val project_svn_tags_url= project_svn_url + "/tags"
+  val project_svn_commiter_url= project_svn_trunk_url.replaceFirst("http","https")
+  
+  val project_maven_groupId= "org.apache.activemq"
+  val project_maven_artifactId= "apollo-broker"
+
+  val website_base_url= "http://activemq.apache.org/apollo"
+}
---
+++ apollo-website/src/main/webapp/_navigation.ssp.md
@@ -0,0 +1,10 @@
+
+* [${project_name} ${project_version}](${uri("/")}) 
+* [Source](${uri("/community/source.html")})
+* [Community](${uri("/community/index.html")})
+* [Support](${uri("/community/support.html")})
+<!-- 
+* [Download](${uri("/download.html")}) 
+* [Documentation](${uri("/documentation/index.html")}) 
+-->
+
