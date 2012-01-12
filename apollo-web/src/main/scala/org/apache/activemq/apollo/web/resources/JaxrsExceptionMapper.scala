931629665b5fc297f3edbee47643f294ed26543a
Hiram R. Chirino <chirino@apache.org>
2012-01-12 15:39:36 +0000

    Fixes issue with CSS not being served up properly.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-apollo/trunk@1230596 13f79535-47bb-0310-9956-ffa450edef68

--- apollo-web/src/main/scala/org/apache/activemq/apollo/web/resources/JaxrsExceptionMapper.scala
+++ apollo-web/src/main/scala/org/apache/activemq/apollo/web/resources/JaxrsExceptionMapper.scala
@@ -42,9 +42,7 @@ class JaxrsExceptionMapper extends ExceptionMapper[Throwable] {
     http_request.getRequestURI + Option(query).map("?"+_).getOrElse("")
   }
 
-  @Produces(Array(APPLICATION_JSON, APPLICATION_XML, TEXT_XML))
   def toResponse(error: Throwable): Response = {
-
     def response(status: Response.Status, msg: String=null) = {
       val response = Response.status(status)
       var dto = new ErrorDTO()
@@ -57,20 +55,25 @@ class JaxrsExceptionMapper extends ExceptionMapper[Throwable] {
 
     error match {
       case ex:WebApplicationException =>
-        var code = ex.getResponse.getStatus
-        if(code >= 400 && code != 401) {
-          if(ex.getResponse.getStatus >= 500) {
-            Resource.warn(ex, "HTTP request from '%s' for %s '%s' caused internal server error: %s", http_request.getRemoteAddr, http_request.getMethod, requested_uri, ex.toString);
-          }
-          var status = Status.fromStatusCode(ex.getResponse.getStatus)
-          ex.getResponse.getEntity match {
-            case null => response(status)
-            case x:String => response(status, x)
-            case _ => ex.getResponse
-          }
-        } else {
+        if ( ! http_request.getServletPath.startsWith("/broker") ) {
           ex.getResponse
+        } else {
+          var code = ex.getResponse.getStatus
+          if(code >= 400 && code != 401) {
+            if(ex.getResponse.getStatus >= 500) {
+              Resource.warn(ex, "HTTP request from '%s' for %s '%s' caused internal server error: %s", http_request.getRemoteAddr, http_request.getMethod, requested_uri, ex.toString);
+            }
+            var status = Status.fromStatusCode(ex.getResponse.getStatus)
+            ex.getResponse.getEntity match {
+              case null => response(status)
+              case x:String => response(status, x)
+              case _ => ex.getResponse
+            }
+          } else {
+            ex.getResponse
+          }
         }
+
       case ex:Throwable =>
         Resource.warn(ex, "HTTP request from '%s' for %s '%s' caused internal server error: %s", http_request.getRemoteAddr, http_request.getMethod, requested_uri, ex.toString);
         response(INTERNAL_SERVER_ERROR, ex.toString)
