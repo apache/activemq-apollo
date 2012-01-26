4ba959958549b441ca1334257f70b60e52bf76b8
Hiram R. Chirino <chirino@apache.org>
2012-01-26 00:05:56 +0000

    Add a customized apollo openwire generator.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-apollo/trunk@1235993 13f79535-47bb-0310-9956-ffa450edef68

---
+++ apollo-openwire-generator/src/main/scala/org/apache/activemq/apollo/openwire/generator/GeneratorTask.scala
@@ -0,0 +1,79 @@
+package org.apache.activemq.apollo.openwire.generator
+
+import java.io.File
+import org.apache.tools.ant.BuildException
+import org.apache.tools.ant.Project
+import org.apache.tools.ant.Task
+import org.codehaus.jam.JamService
+import org.codehaus.jam.JamServiceFactory
+import org.codehaus.jam.JamServiceParams
+import org.apache.activemq.openwire.tool.{JavaTestsGenerator, JavaMarshallingGenerator}
+import reflect.BeanProperty
+
+
+/**
+ * <p>
+ * </p>
+ *
+ * @author <a href="http://hiramchirino.com">Hiram Chirino</a>
+ */
+object GeneratorTask {
+  def main(args: Array[String]): Unit = {
+    var project = new Project
+    project.init
+    var generator = new GeneratorTask
+    generator.setProject(project)
+    if (args.length > 0) {
+      generator.version = Integer.parseInt(args(0))
+    }
+    if (args.length > 1) {
+      generator.sourceDir = new File(args(1))
+    }
+    if (args.length > 2) {
+      generator.targetDir = new File(args(2))
+    }
+    generator.execute
+  }
+}
+
+/**
+ * <p>
+ * </p>
+ *
+ * @author <a href="http://hiramchirino.com">Hiram Chirino</a>
+ */
+class GeneratorTask extends Task {
+
+  @BeanProperty
+  var version = 2
+
+  @BeanProperty
+  var sourceDir = new File("./src/main/scala")
+
+  @BeanProperty
+  var targetDir = new File("./src/main/scala")
+
+  override def execute: Unit = {
+    try {
+
+      println("Parsing source files in: " + sourceDir)
+      var jamServiceFactory = JamServiceFactory.getInstance
+      var params = jamServiceFactory.createServiceParams
+      var dirs = Array(sourceDir)
+      params.includeSourcePattern(dirs, "**/*.java")
+
+      var jam = jamServiceFactory.createService(params)
+
+      var script = new ApolloMarshallingGenerator
+      script.setJam(jam)
+      script.setTargetDir(targetDir.getCanonicalPath)
+      script.setOpenwireVersion(version)
+      script.run
+
+    } catch {
+      case e: Exception => throw new BuildException(e)
+    }
+  }
+
+}
+
