4a8ca840cfa1b24e45eba40e2dc7cd308730c930
Hiram R. Chirino <chirino@apache.org>
2012-02-06 15:14:29 +0000

    Adding the new DetectDTO to the jaxb index so it can be found by Jaxb and also add a hash code and equals impl so that config changes in it can be detected.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-apollo/trunk@1241039 13f79535-47bb-0310-9956-ffa450edef68

--- apollo-dto/src/main/java/org/apache/activemq/apollo/dto/DetectDTO.java
+++ apollo-dto/src/main/java/org/apache/activemq/apollo/dto/DetectDTO.java
@@ -36,4 +36,23 @@ public class DetectDTO extends ProtocolDTO {
     @XmlAttribute(name="timeout")
     public Long timeout;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        DetectDTO detectDTO = (DetectDTO) o;
+
+        if (timeout != null ? !timeout.equals(detectDTO.timeout) : detectDTO.timeout != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (timeout != null ? timeout.hashCode() : 0);
+        return result;
+    }
 }
