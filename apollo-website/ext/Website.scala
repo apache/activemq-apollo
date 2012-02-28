b03b5651c0910535fcfa07a43321abfa2c23a803
Hiram R. Chirino <chirino@apache.org>
2012-02-28 23:20:49 +0000

    Prep for a 1.1 release.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-apollo/trunk@1294892 13f79535-47bb-0310-9956-ffa450edef68

--- apollo-website/ext/Website.scala
+++ apollo-website/ext/Website.scala
@@ -34,10 +34,11 @@ object Website {
   val project_forums_url= "http://activemq.2283324.n4.nabble.com/ActiveMQ-Dev-f2368404.html"
   val project_wiki_url= "https://cwiki.apache.org/confluence/display/ACTIVEMQ/Index"
   val project_logo= "/images/project-logo.png"
-  val project_version= "1.0"
-  val project_snapshot_version= "1.1-SNAPSHOT"
+  val project_version= "1.1"
+  val project_snapshot_version= "1.2-SNAPSHOT"
   val project_versions = List(
         project_version,
+        "1.0",
         "1.0-beta6",
         "1.0-beta5",
         "1.0-beta4",
