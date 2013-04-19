d46e63229f321a85a57f275b144ae38990485333
Hiram R. Chirino <chirino@apache.org>
2013-04-19 20:14:33 +0000

    Update stompjms and hawtdispatch versions.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-apollo/trunk@1470018 13f79535-47bb-0310-9956-ffa450edef68

---
+++ apollo-dto/src/main/java/org/apache/activemq/apollo/dto/DispatchQueueMetrics.java
@@ -0,0 +1,11 @@
+package org.apache.activemq.apollo.dto;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chirino
+ * Date: 4/19/13
+ * Time: 2:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DispatchQueueMetrics {
+}
